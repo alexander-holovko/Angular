c485ff29e478be42b0c2a95563f5b4f263e6fd35
alexander <holovko.works@gmail.com>
2020-05-18 23:13:23 +0300

    initial

---
+++ modules/articles/client/config/articles-admin.client.config.js
@@ -0,0 +1,17 @@
+﻿(function () {
+  'use strict';
+
+  // Configuring the Articles Admin module
+  angular
+    .module('articles.admin')
+    .run(menuConfig);
+
+  menuConfig.$inject = ['menuService'];
+
+  function menuConfig(Menus) {
+    Menus.addSubMenuItem('topbar', 'admin', {
+      title: 'Керувати статтями',
+      state: 'admin.articles.list'
+    });
+  }
+}());
---
+++ modules/articles/client/config/articles.client.menus.js
@@ -0,0 +1,25 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('articles')
+    .run(menuConfig);
+
+  menuConfig.$inject = ['menuService'];
+
+  function menuConfig(menuService) {
+    menuService.addMenuItem('topbar', {
+      title: 'Статті',
+      state: 'articles',
+      type: 'dropdown',
+      roles: ['*']
+    });
+
+    // Add the dropdown list item
+    menuService.addSubMenuItem('topbar', 'articles', {
+      title: 'Список статей',
+      state: 'articles.list',
+      roles: ['*']
+    });
+  }
+}());
